d8431e75518d4f26ae156b06163e42021ad57217
myroslavasydoruk <myroslavasydoruk@10.230.43.107>
2018-03-31 17:32:53 -0500

    second try

--- src/FromMira.java
+++ src/FromMira.java
@@ -3,7 +3,7 @@ public class FromMira {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("From mira");
+System.out.println("From mira second time");
 	}
 
 }
